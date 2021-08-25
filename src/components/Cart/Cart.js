87e0caca628ae805cafd793780743c25235a54d6
kastriot96 <79404857+kastriot96@users.noreply.github.com>
2021-08-25 23:20:27 +0200

    update

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -2,7 +2,7 @@ import { useContext } from "react";
 
 import classes from "./Cart.module.css";
 import Modal from "../UI/Modal";
-import CartItem from './CartItem';
+import CartItem from "./CartItem";
 import CartContext from "../../store/cart-context";
 
 const Cart = (props) => {
@@ -11,14 +11,25 @@ const Cart = (props) => {
   const totalAmount = `$${cartCtx.totalAmount.toFixed(2)}`;
   const hasItems = cartCtx.items.length > 0;
 
-  const cartItemRemoveHandler = id => {};
+  const cartItemRemoveHandler = (id) => {
+    cartCtx.removeItem(id);
+  };
 
-  const cartItemAddHandler = item => {};
+  const cartItemAddHandler = (item) => {
+    cartCtx.addItem({ ...item, amount: 1 });
+  };
 
   const cartItems = (
     <ul className={classes["cart-items"]}>
       {cartCtx.items.map((item) => (
-       <CartItem key={item.id} name={item.name} amount={item.amount} price={item.price} onRemove={} />
+        <CartItem
+          key={item.id}
+          name={item.name}
+          amount={item.amount}
+          price={item.price}
+          onRemove={cartItemRemoveHandler.bind(null, item.id)}
+          onAdd={cartItemAddHandler.bind(null, item)}
+        />
       ))}
     </ul>
   );
